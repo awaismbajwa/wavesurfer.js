e7aa5f3768f75ff6adcfd63e5794402a361904f8
Tim Fischbach <tfischbach@codevise.de>
2017-06-21 19:18:31 +0200

    fixup! Add getters and setters for height and color options

--- src/wavesurfer.js
+++ src/wavesurfer.js
@@ -355,7 +355,7 @@ var WaveSurfer = {
 
     setHeight: function(height) {
         this.params.height = height;
-        this.drawer.setHeight(height);
+        this.drawer.setHeight(height * this.params.pixelRatio);
     },
 
     drawBuffer: function () {
